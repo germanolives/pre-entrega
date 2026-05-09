b66e99c1cb82c35518f95dae1f901327cd275258
germanolives <germanolives@gmail.com>
2026-05-09 19:53:17 -0300

    responsive ofertas

--- src/components/Discount/DiscountList.jsx
+++ src/components/Discount/DiscountList.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import { ItemDiscount } from "./ItemDiscount";
 import { Button } from "../common/Button";
 
 export const DiscountList = ({ offer, price, formattedPrice, message }) => {
-
-  const hideShow = () => {
-  
-  };
+  const [show, setShow] = useState(false);
+  const hideShow = () => setShow(!show);
 
   return (
     <>
-      <Button variant="ghost" onClick={hideShow}>{message}</Button>
-      <>
+      <Button variant="ghost" className="md:hidden" onClick={hideShow}>
+        {message}
+      </Button>
+      <p className="hidden md:block font-bold text-gray-600">{message}</p>
+      <div className={`${!show ? "hidden" : "block"} md:block`}>
         {offer.map((item) => (
           <ItemDiscount
             key={item.id}
@@ -19,7 +21,7 @@ export const DiscountList = ({ offer, price, formattedPrice, message }) => {
             {...item}
           />
         ))}
-      </>
+      </div>
     </>
   );
 };
