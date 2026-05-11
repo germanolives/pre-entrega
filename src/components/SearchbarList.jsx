1236b0982608557cf539365402885a492fb10670
germanolives <germanolives@gmail.com>
2026-05-10 21:31:51 -0300

    probando coincidencias en la searchnavbar parte 2

--- src/components/SearchbarList.jsx
+++ src/components/SearchbarList.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import { formatSlug } from "../utils/formatSlug";
 
 export const SearchbarList = ({ data }) => {
   return (
     <ul>
       {data.map((item) => (
-        <li key={item.id}>
-          <Link>{item.title}</Link>
-        </li>
+        <Link
+          to={`/products/${formatSlug(item.category)}/${formatSlug(item.title)}/${item.id}`}
+        >
+          <li key={item.id}>{item.title}</li>
+        </Link>
       ))}
     </ul>
   );
