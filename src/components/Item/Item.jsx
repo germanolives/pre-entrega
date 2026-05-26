96f65f64508580f02a32ce00f360a6b95bed095b
germanolives <germanolives@gmail.com>
2026-05-26 14:00:23 -0300

    normalizando icono de favoritos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -14,20 +14,30 @@ export const Item = ({ item }) => {
     currency: "EUR",
   });
 
-  const { toggleFavorite, isFavorite} = useFavorite();
+  const { toggleFavorite, isFavorite } = useFavorite();
   const formattedPrice = countryPrice.format(item.price);
 
   const favUndofav = () => {
     toggleFavorite(item);
   };
-  
 
   return (
     <article className="grid grid-rows-[auto_auto_1fr_auto] bg-gray-200 p-4 shadow-2xl border border-gray-300 h-full rounded-sm">
+      <div className="flex flex-row justify-between">
+        <Link to={productPath}>
+          <h3 className="text-sm text-black text-left capitalize line-clamp-2 leading-tight px-2 min-h-9 overflow-hidden">
+            {item.title}
+          </h3>
+        </Link>
+        <Button
+          variant="cristal"
+          onClick={favUndofav}
+          className={`rounded-sm ${isFavorite(item) ? "opacity-100 hover:opacity-80" : "opacity-20 hover:opacity-40"}`}
+        >
+          <FavoriteIcon className="w-4 h-4 mb-6 ml-3" />
+        </Button>
+      </div>
       <Link to={productPath}>
-        <h3 className="text-sm text-black text-left capitalize line-clamp-2 leading-tight px-2 min-h-9 overflow-hidden">
-          {item.title}
-        </h3>
         <div className="w-full aspect-square overflow-hidden bg-white border border-gray-100 rounded-sm">
           <ImgWithSkeleton
             image={item.image}
@@ -41,13 +51,6 @@ export const Item = ({ item }) => {
         <span className="text-xl font-bold text-blue-800 mt-auto">
           {formattedPrice}
         </span>
-        <Button
-          variant="cristal"
-          onClick={favUndofav}
-          className={`rounded-sm ${isFavorite(item) ? "opacity-100 hover:opacity-80" : "opacity-20 hover:opacity-40"}`}
-        >
-          <FavoriteIcon className="w-4 h-4" />
-        </Button>
       </div>
     </article>
   );
