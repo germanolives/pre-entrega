c0e91a4c190f1b2d71a915601603e3b2ce8d467d
germanolives <germanolives@gmail.com>
2026-05-28 23:34:55 -0300

    empezando con firebase/firestore

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPkHYkWpduM6bOW3sYTWzWtWeXe4dMciU",
+  authDomain: "talento-tech-react-5645e.firebaseapp.com",
+  projectId: "talento-tech-react-5645e",
+  storageBucket: "talento-tech-react-5645e.firebasestorage.app",
+  messagingSenderId: "292547743567",
+  appId: "1:292547743567:web:cc5a5e094c87cc2b483f3a",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
