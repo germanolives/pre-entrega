cf6633f4543dec36af74caf22dcd1e4f20c7401a
germanolives <germanolives@gmail.com>
2026-06-09 18:29:11 -0300

    modal en delete

--- src/components/Dashboard/ProductItem.jsx
+++ src/components/Dashboard/ProductItem.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import { useProducts } from "../../context/ProductsContext";
 import { useNavigate } from "react-router-dom";
 import { EditIcon, TrashIcon } from "../Icons/index";
 import { Button } from "../common/Button";
 import { Modal } from "../common/Modal";
 
+
 export const ProductItem = ({ item }) => {
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { deleteProduct } = useProducts();
   const countryPrice = new Intl.NumberFormat("en-GB", {
     style: "currency",
     currency: "EUR",
   });
   const formattedPrice = countryPrice.format(item.price);
+  const handleDeleteConfirm = async () => {
+    const res = await deleteProduct(item.id);
+    if (res?.success) {
+      setIsModalOpen(false); // Cerramos el modal de inmediato
+    }
+  };
 
   return (
     <div className="grid grid-cols-1  md:grid-cols-[3fr_5fr_3fr_2fr_2fr_0.75fr_0.75fr] bg-cyan-200 border border-gray-300 p-5 gap-5 rounded-sm">
@@ -33,17 +42,49 @@ export const ProductItem = ({ item }) => {
       <Button
         variant="cristal"
         className="bg-cyan-100 border border-gray-300 rounded-sm flex items-center justify-center"
-        onClick={()=>{navigate(`/dashboard/${item.categorySlug}/${item.titleSlug}/${item.id}`)}}
+        onClick={() => {
+          navigate(
+            `/dashboard/${item.categorySlug}/${item.titleSlug}/${item.id}`,
+          );
+        }}
       >
         <EditIcon className="text-green-500  w-6 h-6" />
       </Button>
       <Button
         variant="cristal"
         className="bg-cyan-100 border border-gray-300 rounded-sm flex items-center justify-center"
-        onClick={()=>deleteProduct(item.id)}
+        onClick={() => setIsModalOpen(true)}
       >
         <TrashIcon className="text-red-400 w-6 h-6" />
       </Button>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Confirm Deletion"
+      >
+        <div className="flex flex-col gap-4">
+          <p className="leading-relaxed">
+            Are you sure you want to permanently delete this product?{" "}
+            <span className="font-bold text-gray-900">"{item.title}"</span>?
+            This action cannot be undone in Firestore.
+          </p>
+
+          <div className="flex justify-end gap-2 mt-2">
+            <Button
+              className="bg-gray-300 text-gray-700 hover:bg-gray-400 text-xxs py-1.5 px-3 rounded-sm uppercase font-bold"
+              onClick={() => setIsModalOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              className="bg-red-600 text-white hover:bg-red-700 text-xxs py-1.5 px-3 rounded-sm uppercase font-bold"
+              onClick={handleDeleteConfirm}
+            >
+              Yes, Delete
+            </Button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
