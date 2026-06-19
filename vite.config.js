39808e5d6df4813964624e602c6d6c0434efbca8
germanolives <germanolives@gmail.com>
2026-06-19 13:58:44 -0300

    vite.config 2

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
+    react({
+      // React Compiler ya está integrado en el plugin de react,
+      // pero puedes asegurarte de que babel solo toque lo necesario
+    }),
+    babel({
+      // 1. Limita el filtro solo a tus archivos de código fuente
+      filter: /\.(jsx|tsx)$/,
+      presets: [reactCompilerPreset()],
+    }),
     tailwindcss(),
   ],
   build: {
     // 1. Ajuste del límite de advertencia (1000kb = 1MB)
     chunkSizeWarningLimit: 1000,
-    
+
     // 2. Estrategia de optimización de archivos (Code Splitting)
     rollupOptions: {
       output: {
