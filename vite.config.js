b1cdeebe9fdf43de776138087bfd4ce52a587211
germanolives <germanolives@gmail.com>
2026-07-01 19:06:34 -0300

    chore: simplificar plugins de vite para destrabar build en vercel

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,19 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 🚀 Al no tener 'base', Vercel asume la raíz "/" por defecto, que es lo que necesita
   plugins: [
     react(),
-    babel({
-      // Cambiamos 'filter' por 'include' (usa un array para archivos jsx/tsx)
-      include: [/\.(jsx|tsx)$/],
-      presets: [reactCompilerPreset()],
-    }),
     tailwindcss(),
   ],
   build: {
-    // 1. Ajuste del límite de advertencia (1000kb = 1MB)
     chunkSizeWarningLimit: 1000,
-
-    // 2. Estrategia de optimización de archivos (Code Splitting)
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // Separa las librerías grandes (node_modules) en un archivo separado
           if (id.includes("node_modules")) {
             return "vendor";
           }
